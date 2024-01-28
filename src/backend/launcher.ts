f08d2b72d19455660e8db8b46d400fb01d06be11
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2024-01-27 21:36:25 -0500

    Support additional gamescope options

--- src/backend/launcher.ts
+++ src/backend/launcher.ts
@@ -211,6 +211,10 @@ async function prepareLaunch(
         }
       }
 
+      gameScopeCommand.push(
+        ...shlex.split(gameSettings.gamescope.additionalOptions)
+      )
+
       // Note: needs to be the last option
       gameScopeCommand.push('--')
     }
