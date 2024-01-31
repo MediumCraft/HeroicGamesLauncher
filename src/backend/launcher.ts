fc2f2c75f8d4652797e571a643317a8bd7beebb1
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2024-01-31 17:22:45 -0500

    review comments

--- src/backend/launcher.ts
+++ src/backend/launcher.ts
@@ -212,7 +212,7 @@ async function prepareLaunch(
       }
 
       gameScopeCommand.push(
-        ...shlex.split(gameSettings.gamescope.additionalOptions)
+        ...shlex.split(gameSettings.gamescope.additionalOptions ?? '')
       )
 
       // Note: needs to be the last option
